87594879a85c9d910466c0018dd8c7c75786e831
Haikalmuchammad02 <73925124+Haikalmuchammad02@users.noreply.github.com>
2021-04-06 21:27:59 +0700

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,7 @@
+print ('___________')
+
+print ('BIODATA KU')
+print ('___________')
+print ('Nama Lengkap : Muchammad Fikri Haikal') 
+print ('Kelas : Teknik Informatika 2018 A')
+print ('Universitas Nahdlatul Ulama Sidoarjo')
